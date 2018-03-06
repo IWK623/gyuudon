471abc723353a9c6fe9cf036e471b18d7a2ef575
keeii <keeii@192.168.1.9>
2018-03-06 17:14:27 +0900

    パーリンノイズ

---
+++ src/main/java/iw/tests/TestCase01.java
@@ -0,0 +1,73 @@
+package iw.tests;
+
+/**
+ * @file TestCase01
+ * @brief (http://kan-kikuchi.hatenablog.com/entry/PerlinNoise)より参照したコード。多分一番シンプル
+ * @author k6!
+ */
+
+import cn.nukkit.block.Block;
+import cn.nukkit.level.Level;
+import cn.nukkit.math.Vector3;
+
+public class TestCase01{
+
+  //! seed値
+  private float _seedX, _seedZ;
+
+  //! 最大高度
+  private float _maxHeight = 10;
+
+  //! 形状の険しさ
+  private float _relief = (float) 15.0;
+
+  /**
+   * 
+   * @brief パーリンノイズをつかった地形の生成
+   * @param level 生成するワールド 
+   * @param sx 生成開始のx座標
+   * @param sz 生成開始のz座標
+   * @param ex 生成終了のx座標
+   * @param ez 生成終了のz座標　
+   * @return なし
+   */
+  public TestCase01 (Level level, int sx, int sz, int ex, int ez) {
+
+    //Seedの生成
+    _seedX = (float) (Math.random() * 100f);
+    _seedZ = (float) (Math.random() * 100f);
+    	
+    for(int x = sx; x <= ex; x++) {
+    	for(int z = sz; z <= ez; z++) {
+    		this.Set(level, x, z);
+        }
+    }
+  }
+  
+
+  private void Set(Level level,int x,int z){
+    float y = 0;
+
+    float xSample = (x + _seedX) / _relief;
+    float zSample = (z + _seedZ) / _relief;
+    
+    //パーリンノイズは 0.0 ~ 1.0 の値を返す
+    float noise = (float) Noise.noise(xSample, y, zSample);
+
+    y = _maxHeight * noise;
+
+
+    for(;y >= 0; y--) {
+        if(y > _maxHeight * 0.3f){
+            level.setBlock(new Vector3(x,y,z),Block.get(Block.DIRT));
+          }
+          else {
+            level.setBlock(new Vector3(x,y,z),Block.get(Block.STONE));
+        }    	
+    }
+
+    
+
+  }
+
+}
--- src/main/java/ygr/gyuudon/Main.java
+++ src/main/java/ygr/gyuudon/Main.java
@@ -1,23 +1,35 @@
 
 package ygr.gyuudon;
 
-import cn.nukkit.Player;
-import cn.nukkit.block.Block;
 import cn.nukkit.command.Command;
 import cn.nukkit.command.CommandSender;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.Listener;
-import cn.nukkit.event.player.PlayerInteractEvent;
+import cn.nukkit.event.player.PlayerChatEvent;
 import cn.nukkit.plugin.PluginBase;
-import cn.nukkit.utils.TextFormat;
+import iw.tests.TestCase01;
 
 public class Main extends PluginBase implements Listener{
 
 	 public void onEnable() {
-	        getLogger().info("起動しました");
-	        this.getServer().getPluginManager().registerEvents(this, this);//必須
+	        getLogger().info("牛丼 is Enable");
+	        this.getServer().getPluginManager().registerEvents(this, this);
 	 }
-
-	 //TestComit してみたよ3
+	 
+	 @EventHandler
+	 public void onChat(PlayerChatEvent ev) {
+		 if(ev.getMessage() == "test01") {
+			 this.getServer().broadcastMessage("TestCase01 is Runing");
+			 new TestCase01(ev.getPlayer().getLevel(),0,0,255,255);
+		 }
+	 }
+	 
+	 
+	 public boolean onCommand(final CommandSender sender, Command command, String label, String[] args){
+		switch(command.getName()){
+			
+		}
+		return false;
+	 }
 	 
 }
